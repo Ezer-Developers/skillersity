ee1a8dd8cd68df5422fcf3b870b82435ad9acfed
Ezer Developers <ezerdevelopers@gmail.com>
2024-12-16 12:30:00 +0530

    Contact details

--- app/(pages)/contact/(contact)/index.js
+++ app/(pages)/contact/(contact)/index.js
@@ -43,7 +43,7 @@ const ContactPage = () => {
           <div className="rbt-google-map bg-color-white rbt-section-gapTop">
             <iframe
               className="w-100"
-              src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d2965.0824050173574!2d-93.63905729999999!3d41.998507000000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sWebFilings%2C+University+Boulevard%2C+Ames%2C+IA!5e0!3m2!1sen!2sus!4v1390839289319"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3928.6303677357487!2d76.33133351022508!3d10.047329090019096!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b080d8ad2436683%3A0x39e1ac3beda45ba3!2sEVOXIGN%20Technologies%20Pvt.%20Ltd!5e0!3m2!1sen!2sin!4v1734332234617!5m2!1sen!2sin"
               height="600"
               style={{ border: "0" }}
             ></iframe>
